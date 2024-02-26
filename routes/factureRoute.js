91a1e8b304e077bed60566f846d9ad4f18e0f2a6
Stephane Koeniguer <103316525+StephaneKoeniguer@users.noreply.github.com>
2024-02-26 20:57:41 +0100

    Update factureRoute.js

--- routes/factureRoute.js
+++ routes/factureRoute.js
@@ -2,13 +2,16 @@ const express = require("express");
 const router = express.Router();
 const factureController = require("../controller/factureController");
 
-// affiche toutes les factures
+// Affiche toutes les factures
 router.get("/factures", factureController.fetchAll);
 
+// Ajoute une facture
 router.post("/factures", factureController.add);
 
+// Mettre à jour une facture
 router.put("/factures/:id", factureController.update);
 
+// Supprimer une facture
 router.delete("/factures/:id", factureController.delete)
 
 module.exports = router;
